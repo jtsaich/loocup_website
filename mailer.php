3ed43abc074936fe31234cb512cd180ae642f807
Daniel W. Chen <dnnl8017@gmail.com>
2015-06-12 15:41:22 +0800

    adding php for support and changed images for smoke

---
+++ mailer.php
@@ -0,0 +1,57 @@
+<?php
+/* Set e-mail recipient */
+$myemail = "loocupapp@gmail.com";
+
+/* Check all form inputs using check_input function */
+$email = check_input($_POST['email']);
+$message = check_input($_POST['message'], "Write your message");
+
+/* If e-mail is not valid show error message */
+if (!preg_match("/([\w\-]+\@[\w\-]+\.[\w\-]+)/", $email))
+{
+show_error("E-mail address not valid");
+}
+/* Let's prepare the message for the e-mail */
+$message = "
+
+Message:
+$message
+
+";
+
+/* Send the message using mail() function */
+mail($myemail, $message);
+
+/* Redirect visitor to home page */
+header('Location: index.html');
+exit();
+
+/* Functions we used */
+function check_input($data, $problem='')
+{
+$data = trim($data);
+$data = stripslashes($data);
+$data = htmlspecialchars($data);
+if ($problem && strlen($data) == 0)
+{
+show_error($problem);
+}
+return $data;
+}
+
+function show_error($myError)
+{
+?>
+<html>
+<body>
+
+<p>Please correct the following error:</p>
+<strong><?php echo $myError; ?></strong>
+<p>Hit the back button and try again</p>
+
+</body>
+</html>
+<?php
+exit();
+}
+?>
