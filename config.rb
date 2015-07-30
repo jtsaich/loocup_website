aac610ba716e685483cf17d26225982c4ffe1847
tsai0606 <jtsai0606@gmail.com>
2015-07-29 21:27:51 -0700

    improved overall website experience, especially viewing from mobile

--- config.rb
+++ config.rb
@@ -1,12 +1,16 @@
 require 'compass/import-once/activate'
 # Require any additional compass plugins here.
 
+
+
 # Set this to the root of your project when deployed:
 http_path = "/"
 css_dir = "css"
 sass_dir = "scss"
+add_import_path "bower_components/bootstrap-sass/assets/stylesheets"
 images_dir = "images"
 javascripts_dir = "js"
+
 
 # You can select your preferred output style here (can be overridden via the command line):
 # output_style = :expanded or :nested or :compact or :compressed
